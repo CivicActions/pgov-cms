1a0dbdd5eee31914982ac920c15ff44e0ee5799f
Adrienne <acabouet@protonmail.com>
2024-11-20 02:41:07 -0500

    Setting up uswds

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const path = require("path");
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true
+  },
+  sassOptions: {
+    includePaths: [
+      path.join(__dirname, "node_modules", "@uswds", "uswds", "packages"),
+    ],
+  }
 };
 
 export default nextConfig;
